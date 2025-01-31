9741966f5d6b0eed17d81693ed158a5e92c2c66d
taisan11 <131413638+taisan11@users.noreply.github.com>
2025-01-31 17:36:43 +0900

    update version 0.1.0β

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -2,12 +2,11 @@ import { blue } from "@std/fmt/colors";
 
 function about(lang: string) {
   console.log(
-    `OS-Wasm ${blue("v0.0.2β")}\nBaseSystem: ${blue(Deno.build.os)}\nDeno: ${
+    `OS-Wasm ${blue("v0.1.0β")}\nBaseSystem: ${blue(Deno.build.os)}\nDeno: ${
       blue(Deno.version.deno)
     }\nV8: ${blue(Deno.version.v8)}\nTypeScript: ${
       blue(Deno.version.typescript)
     }\n`,
   );
 }
-
 export { about };
